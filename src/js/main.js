8a2093a6b2a2079bc9f0049cafcc14b480550666
irenemanas <irenemanas@gmail.com>
2021-02-08 20:18:47 +0100

    paint

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const showsContainer = document.querySelector('.js-shows-container');
 let shows;
 
 
@@ -10,6 +11,7 @@ function getDataFromApi() {
     .then(response => response.json())
     .then(data => {
       shows = data;
+      paintShows();
       setInLocalStorage();
     });
 
@@ -30,10 +32,25 @@ function getFromLocalStorage() {
   } else {
     const arrayShows = JSON.parse(localStorageShows);
     shows = arrayShows;
-    console.log(shows);
+    paintShows();
   }
 }
 
+// PAINT
+
+function paintShows() {
+  console.log('pintando.....', shows);
+  let htmlCode = ``;
+  for (const show of shows) {
+    console.log(show.show);
+    htmlCode += `<li class="show">`;
+    htmlCode += `<h2 class="show__title">${show.show.name}</h2>`;
+    htmlCode += `<img src="http://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg" alt="Cartel Serie"></img>`;
+    htmlCode += `<li class="show">`;
+
+  }
+  showsContainer.innerHTML = htmlCode;
+}
 
 
 // START APP
