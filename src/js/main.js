003865e57a2d5e24eee649502f06d120965d7451
irenemanas <irenemanas@gmail.com>
2021-02-08 19:38:56 +0100

    add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-console.log('>> Ready :)');
+let shows;
+
+
+// API
+
+function getDataFromApi() {
+  fetch('http://api.tvmaze.com/search/shows?q=girls')
+    .then(response => response.json())
+    .then(data => {
+      shows = data;
+      setInLocalStorage();
+    });
+
+}
+
+// LOCAL STORAGE
+
+function setInLocalStorage() {
+  const stringShows = JSON.stringify(shows);
+  localStorage.setItem('data', stringShows);
+
+}
+
+function getFromLocalStorage() {
+  const localStorageShows = localStorage.getItem('data');
+  if (localStorageShows === null) {
+    getDataFromApi();
+  } else {
+    const arrayShows = JSON.parse(localStorageShows);
+    shows = arrayShows;
+    console.log(shows);
+  }
+}
+
+
+
+// START APP
+
+getFromLocalStorage();
